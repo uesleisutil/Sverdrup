150c87f7c6d68789aec8250b2c5f1e6baa143e7f
uesleisutil <ueslei@outlook.com>
2020-08-27 16:03:46 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,165 @@
-
-#include "RTClib.h"
 #include <SD.h>
-#include <Adafruit_BME280.h>
+#include "SPI.h"
+#include "DHT.h"
+#include <RTClibExtended.h>
+#include <Wire.h>
+#include <LowPower.h>
+#include <stdlib.h>
+#include <avr/interrupt.h> 
+#include <avr/sleep.h> 
+#include <avr/power.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <./Narcoleptic.h>
-#include <DHT.h>
 
-// SD card resources.
-int CS_SD = 10;
-
-// File resources.
-File file;
-String dataMessage;
-
-// RTC resources.
-RTC_DS1307 rtc;
-char gettime[20];
-
-// BME280 resources.
-float temp_bme280;
-float rh_bme280;
-float pres_bme280; 
-Adafruit_BME280 bme;
-
-// DS18b20 resources.
-float temp_ds18b20;
-#define ONE_WIRE_BUS 8
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
-// DHT22 resources.
-float temp_dht22;
-float rh_dht22;
-#define DHTPIN 2
+// DHT22 sensor.
+#define DHTPIN 7
 #define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE); 
- 
-// LED resources.
-const int led = 9;
+DHT dht(DHTPIN, DHTTYPE);
 
-// Sleep time (milliseconds).
-// 3600000 (every hour) - 148000 (time needed by the program to run)
-#define SLEEP_TIME 3452000
+// DS18B20 module.
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); 
 
-// -- Do not change above --
-void setup() {
-  Serial.begin(57600);
+// RTC module.
+#define wakePin 2
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const int chipSelect = 10;
+int interval_sec = 0; //Alarm interval in seconds
+int interval_min = 1; //Alarm interval in minutes
+int interval_hr = 00; //Alarm interval in hours
+char file_name[25] = "Readings.csv";
+
+// Led.
+const int led = 8;
+
+void setup () {
+  Serial.begin(9600);
   delay(3000);
-  // LED.
+
+  // Led. 
   pinMode(led, OUTPUT);
 
-  //Start SD card.
-  if (!SD.begin(CS_SD))
-  {
-    Serial.println("SD card failed!");
-    return;
-  }
-  Serial.println("SD card ok.");
-  Serial.println();
-
-  //Check RTC.
-  if (! rtc.begin())
-  {
-    Serial.println("RTC not found.!");
+  // RTC.
+  if (! rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+    digitalWrite(led, HIGH);
+    delay(5000);
+    digitalWrite(led, LOW);   
     while (1);
   }
-  if (! rtc.isrunning())
-  {
-    Serial.println("RTC not running!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    //Format: DateTime(year, month, day, hour, minute, second)
-    //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  // Init sensors.
-  bme.begin(0x76);
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  pinMode(wakePin, INPUT_PULLUP);
+  rtc.armAlarm(1, false);
+  rtc.clearAlarm(1);
+  rtc.alarmInterrupt(1, false);
+  rtc.armAlarm(2, false);
+  rtc.clearAlarm(2);
+  rtc.alarmInterrupt(2, false);
+  rtc.writeSqwPinMode(DS3231_OFF);
+
+  //Initialize DS18B20, DHT22 sensors.
+  sensors.begin();
   dht.begin();
-  delay(3000);
+
+  // SD Card.
+  pinMode(10, OUTPUT);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    digitalWrite(led, HIGH);
+    delay(5000);
+    digitalWrite(led, LOW);   
+    while (1);
+  }
+  SD.remove(file_name);
+  File dataFile = SD.open(file_name, FILE_WRITE);
+  dataFile.println("Date,Hour,Minute,DHT22_T,DHT22_H,DS18B20_T"); 
+  dataFile.close();
+  delay(100);
 }
 
-void loggingDS18B20() {
-  sensors.requestTemperatures(); 
-  temp_ds18b20 = sensors.getTempCByIndex(0); // Temperature in Celsius
+void wakeUp() {
+ 
 }
 
-void loggingBME280() {
-  temp_bme280 = bme.readTemperature();
-  rh_bme280   = bme.readHumidity();
-  pres_bme280 = bme.readPressure() / 100.0F;
-}
-
-void loggingDHT22() {
-  temp_dht22 = dht.readTemperature();
-  rh_dht22   = dht.readHumidity();
-}
-
-void savingdata() {
-  file=SD.open("data.txt", FILE_WRITE);
+void loop () {
+  // Print time.
   DateTime now = rtc.now();
-  sprintf(gettime, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
-  dataMessage = String(gettime) +"," + String(temp_ds18b20) + "," + String(temp_bme280) + 
-                  "," + String(rh_bme280) + "," + String(pres_bme280) + "," +
-                   String(temp_dht22) + "," +  String(rh_dht22) + "\r\n";
-  file.print(dataMessage);
-  file.close();
-}
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
 
-void printingdata() {
-  Serial.println(gettime);
-  Serial.print(" DS18B20: ");
-  Serial.print(temp_ds18b20);
-  Serial.print(" BME_temp: ");
-  Serial.print(temp_bme280);
-  Serial.print(" BME_rh: ");
-  Serial.print(rh_bme280);
-  Serial.print(" BME_pres: ");
-  Serial.print(pres_bme280);
-  Serial.print(" DHT_temp: ");
-  Serial.print(temp_dht22);  
-  Serial.print(" DHT_rh: ");
-  Serial.print(rh_dht22);  
-  Serial.println();  
-}
-
-void loop() {
-  loggingDS18B20();
-  loggingBME280();
-  loggingDHT22(); 
-  savingdata();
-  printingdata();
-  digitalWrite(led, HIGH);
+  // Get data from DHT22, check it and print.
   delay(5000);
-  digitalWrite(led, LOW);
-  delay(500); 
+  float dht22_h = dht.readHumidity();
+  float dht22_t = dht.readTemperature();
+    if (isnan(dht22_h) || isnan(dht22_t)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      digitalWrite(led, HIGH);
+      delay(5000);
+      digitalWrite(led, LOW);   
+      return;
+    }
+  Serial.print(("DHT22 H: "));
+  Serial.print(dht22_h);
+  Serial.print((" %\t"));
+  Serial.print(("DHT22 T: "));
+  Serial.print(dht22_t);
+  Serial.print((" *C\t"));
+  delay(3000);
+
+  // Get data from DS18B20, check it and print.
+  sensors.requestTemperatures();
+  float ds18b20_t =sensors.getTempCByIndex(0);
+  if (isnan(ds18b20_t)) {
+    Serial.println(F("Failed to read from DS18B20 sensor!"));
+    digitalWrite(led, LOW);
+    delay(1000);
+    digitalWrite(led, HIGH);
+    delay(2000);
+    return;
+  }    
+  Serial.print("DS18B20 T: ");
+  Serial.print(ds18b20_t);  
+  Serial.print((" *C\t"));
+  delay(3000);
+
+  // SD card.
+  File dataFile = SD.open(file_name, FILE_WRITE);
+  String dataString = now.day() + String("/") + now.month() + String("/") + now.year() + String(",") + now.hour() + String(",") + now.minute() + String(",") + dht22_t + String(",") + dht22_h + String(",") + ds18b20_t;
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+  else {
+    Serial.println(F("error opening file to log data"));
+    digitalWrite(led, HIGH);
+    delay(5000);
+    digitalWrite(led, LOW);   
+    return;
+  }
+
+  // Sleep from RTC
+  DateTime nextAlarm = now + TimeSpan(0, interval_hr, interval_min, interval_sec);
+  Serial.print("Alarm at: ");
+  Serial.print(nextAlarm.hour());
+  Serial.print(":");    
+  Serial.print(nextAlarm.minute());
+  Serial.print(" ");     
+  Serial.println(nextAlarm.second());
+
   digitalWrite(led, HIGH);
   delay(500);
   digitalWrite(led, LOW);
@@ -140,5 +167,22 @@ void loop() {
   digitalWrite(led, HIGH);
   delay(500);
   digitalWrite(led, LOW);
-  Narcoleptic.delay(SLEEP_TIME);
+  delay(500); 
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);  
+  digitalWrite(led, HIGH);
+  delay(500);
+  rtc.setAlarm(ALM1_MATCH_HOURS, nextAlarm.second(), nextAlarm.minute(), nextAlarm.hour (), 1);
+  rtc.alarmInterrupt(1, true);
+  delay(3000);
+  attachInterrupt(0, wakeUp, LOW);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  detachInterrupt(0);
+  rtc.armAlarm(1, false);
+  rtc.clearAlarm(1);
+  rtc.alarmInterrupt(1, false);
+  Serial.println();
+  delay(3000);
 }
